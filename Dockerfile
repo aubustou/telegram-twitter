6b5cb93d23d9d145a83272995a7552a505096992
aubustou <charlemagnecontrelesninjas@gmail.com>
2022-08-08 20:14:17 +0200

    add packaging

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# syntax=docker/dockerfile:1
+
+FROM python:3.10-slim-buster
+
+WORKDIR /app
+
+
+COPY . .
+RUN pip3 install .
+
+CMD ["telegram-twitter-bot"]
---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import find_packages, setup
+
+VERSION = '1.0'
+
+setup(
+    name="telegram-twitter",
+    version=VERSION,
+    packages=find_packages(exclude=['tests']),
+    author='Aubustou',
+    author_email='survivalfr@yahoo.fr',
+    description="A Telegram bot to post tweets to a Twitter account",
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'telegram-twitter-bot = telegram_twitter:main',
+        ]
+    },
+    install_requires=[
+        "python-telegram-bot",
+        "tweepy"
+    ],
+)
